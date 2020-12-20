b4d9c9973c359a8c2160a7f72ba560c0bf2a45f4
brunodsp <drakearcanjo@gmail.com>
2020-12-19 21:50:30 -0300

    undo

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
-import utilStyles from '../styles/utils.module.css'
 
 export default function FirstPost() {
   return (
     <>
-    <Head className={utilStyles.headingMd}>
+    <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
