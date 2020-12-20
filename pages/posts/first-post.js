94d919a8ac083527a90ff9df37aaa5a0f92deb99
brunodsp <drakearcanjo@gmail.com>
2020-12-19 21:46:09 -0300

    updated styel from first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function FirstPost() {
   return (
     <>
-    <Head>
+    <Head className={utilStyles.headingMd}>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
